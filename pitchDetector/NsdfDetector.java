bfc08c215b8ef4033dee6af27bc91ef231d3bc84
marcallen <marcallen@0c29aa44-673a-11de-8ec2-fbb090c18dad>
2007-07-20 05:12:26 +0000

    Changed List to Vector for ME compatibilty

--- pitchDetector/NsdfDetector.java
+++ pitchDetector/NsdfDetector.java
@@ -1,22 +1,21 @@
 package pitchDetector;
 
 import soundDevice.SoundDevice;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.Vector;
 
 public class NsdfDetector extends PitchDetector {
 	
 	private double[] m_nsdf = null; 
 	private double[] m_dnsdf = null;
-	private LinkedList m_maxima = null;
-
+	private Vector m_maxima = null;
+	
 	public NsdfDetector(SoundDevice soundDevice) throws PitchDetectorException {
 		super(soundDevice);
 		
 		int frameSize = getSoundDevice().getSoundInfo().getFrameSize();
 		m_nsdf = new double[frameSize];
 		m_dnsdf = new double[frameSize];
-		m_maxima = new LinkedList();
+		m_maxima = new Vector();
 	}
 	
 	public void calcPitch() throws PitchDetectorException {		
@@ -51,7 +50,7 @@ public class NsdfDetector extends PitchDetector {
 		double highestPeak = 0;
 		for (int i = 0; i < m_dnsdf.length - 1; ++i) {
 			if (m_dnsdf[i] > 0 && m_dnsdf[i + 1] < 0) {
-				m_maxima.addLast(new Maxima(i, m_nsdf[i]));
+				m_maxima.add(new Maxima(i, m_nsdf[i]));
 				if (m_nsdf[i] > highestPeak) {
 					highestPeak = m_nsdf[i];
 				}
@@ -59,9 +58,8 @@ public class NsdfDetector extends PitchDetector {
 		}
 		
 		setPitch(-1);
-		ListIterator i = m_maxima.listIterator();
-		while (i.hasNext()) {
-			Maxima m = (Maxima)i.next();
+		for (int i = 0; i < m_maxima.size(); ++i) {
+			Maxima m = (Maxima)m_maxima.get(i);
 			if (m.getValue() > 0.9 * highestPeak) {
 				setPitch(getSoundDevice().getSoundInfo().getSampleRate() / m.getIndex());
 				break;
